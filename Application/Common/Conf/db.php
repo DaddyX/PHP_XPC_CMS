ed95a1e7fe22f3979670a3786ddac93db1aad9b6
zhouxuan <zhou2315@qq.com>
2017-04-26 16:37:54 +0800

    update login bg_image

--- Application/Common/Conf/db.php
+++ Application/Common/Conf/db.php
@@ -1,12 +1,22 @@
 <?php
 return array(
-	/* 数据库设置 */
-    'DB_TYPE'               =>  'mysql',     // 数据库类型
-	'DB_HOST'               =>  '127.0.0.1', // 服务器地址
-    'DB_NAME'               =>  'xpc',          // 数据库名
-	'DB_USER'               =>  'root',      // 用户名
-	'DB_PWD'                =>  '!1a^465#465ASD$',          // 密码
-    'DB_PORT'               =>  '3306',        // 端口
-    'DB_PREFIX'             =>  'db_',    // 数据库表前缀
-    'DB_CHARSET'            =>  'utf8',      // 数据库编码默认采用utf8
+		/* 数据库设置 */
+	    'DB_TYPE'               =>  'mysql',     // 数据库类型
+		'DB_HOST'               =>  '127.0.0.1', // 服务器地址
+	    'DB_NAME'               =>  'xpc',          // 数据库名
+		'DB_USER'               =>  'root',      // 用户名
+		'DB_PWD'                =>  '!1a^465#465ASD$',          // 密码
+	    'DB_PORT'               =>  '3306',        // 端口
+	    'DB_PREFIX'             =>  'db_',    // 数据库表前缀
+	    'DB_CHARSET'            =>  'utf8',      // 数据库编码默认采用utf8
+    
+// 		/* 数据库设置 */
+// 		'DB_TYPE'               =>  'mysql',     // 数据库类型
+// 		'DB_HOST'               =>  'qdm159258169.my3w.com', // 服务器地址
+// 		'DB_NAME'               =>  'qdm159258169_db',          // 数据库名
+// 		'DB_USER'               =>  'qdm159258169',      // 用户名
+// 		'DB_PWD'                =>  'zxcv0211',          // 密码
+// 		'DB_PORT'               =>  '3306',        // 端口
+// 		'DB_PREFIX'             =>  'db_',    // 数据库表前缀
+// 		'DB_CHARSET'            =>  'utf8',      // 数据库编码默认采用utf8
 );
