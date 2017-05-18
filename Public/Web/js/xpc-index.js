898a072addb4de866d37900d47d0308e3e279bbc
zhouxuan <zhou2315@qq.com>
2017-05-18 16:30:43 +0800

    update Index/index.html

--- Public/Web/js/xpc-index.js
+++ Public/Web/js/xpc-index.js
@@ -1,48 +1,7 @@
-//目录树折叠按钮
-function setTreeStyle(obj) {
-	var objStyle = obj.children("b");
-	var objList = obj.siblings("ul");
-	if(objList.length == 1) {
-		var style = objStyle.attr("class");
-		objStyle.attr("class", "On2Off");
-		setTimeout(
-			function() {
-				if(style == "Off") {
-					objList.parent().siblings("li").children("span").children(".On").each(function() {
-						setTreeStyle($(this).parent());
-					});
-					var H = objList.innerHeight()
-					objList.css({
-						display: "block",
-						height: "0"
-					});
-					objList.animate({
-						height: H
-					}, 300, function() {
-						$(this).css({
-							height: "auto"
-						});
-					});
-					objStyle.attr("class", "On");
-				} else if(style == "On") {
-					var H = objList.innerHeight()
-					objList.animate({
-						height: 0
-					}, 300, function() {
-						$(this).css({
-							height: "auto",
-							display: "none"
-						})
-					});
-					objStyle.attr("class", "Off");
-				}
-			},
-			40
-		);
-	}
-}
 
-//初始化时间
+/**
+ * 初始化时间
+ */
 function initTimeWidget(obj) {
 	var timestamp = (Date.parse(new Date())).toString();
 	var timeInt = parseInt(timestamp.substring(0, 10));
