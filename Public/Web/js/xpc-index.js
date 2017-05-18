f05428ec43e089aade6f7585ead2e196e802e4d5
zhouxuan <zhou2315@qq.com>
2017-05-18 19:00:16 +0800

    update Index/index.html

--- Public/Web/js/xpc-index.js
+++ Public/Web/js/xpc-index.js
@@ -1,4 +1,14 @@
-
+/**
+ * 重写时间格式化方法
+ */
+Date.prototype.toString = function() {
+	return this.getFullYear() +
+		"-" + (this.getMonth() > 8 ? (this.getMonth() + 1) : "0" + (this.getMonth() + 1)) +
+		"-" + (this.getDate() > 9 ? this.getDate() : "0" + this.getDate()) +
+		" " + (this.getHours() > 9 ? this.getHours() : "0" + this.getHours()) +
+		":" + (this.getMinutes() > 9 ? this.getMinutes() : "0" + this.getMinutes()) +
+		":" + (this.getSeconds() > 9 ? this.getSeconds() : "0" + this.getSeconds());
+}
 /**
  * 初始化时间
  */
